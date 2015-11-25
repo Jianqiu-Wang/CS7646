6f81422ccefcf7f0f019e0f92f0cad010f6bff04
Alex Milkov <amilkov3@gmail.com>
2015-11-25 06:09:58 -0500

    cleanup

--- Week_1/__init__.py
+++
@@ -1,1 +0,0 @@
-__author__ = 'amilkov3'
---
+++ mc3_p2/code.py
@@ -0,0 +1,34 @@
+__author__ = 'amilkov3'
+
+import os
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir=os.path.join("..", "data")):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                              parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df = df.join(df_temp)
+
+    return df
+
+
+if __name__ == '__main__':
+
+    dates = pd.date_range('2008-01-01', '2009-12-31')
+
+    symbols = ['IBM', 'ML4T-399']
+
+    df = get_data(symbols, dates)
+
+
+
+
+
